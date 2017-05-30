49607df23fa57b123193bcc0724162e9ca3ef8ba
Corcis <corcisdr@gmail.com>
2017-05-29 23:05:05 -0500

    Add files via upload

--- pizzeria.sql
+++ pizzeria.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: May 18, 2017 at 11:34 PM
--- Server version: 10.1.16-MariaDB
--- PHP Version: 7.0.9
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 30-05-2017 a las 06:02:06
+-- Versión del servidor: 10.1.21-MariaDB
+-- Versión de PHP: 7.1.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,14 +17,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `pizzeria`
+-- Base de datos: `pizzeria`
 --
-CREATE DATABASE pizzeria;
-use pizzeria;
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `categoria`
+-- Estructura de tabla para la tabla `categoria`
 --
 
 CREATE TABLE `categoria` (
@@ -34,7 +33,7 @@ CREATE TABLE `categoria` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `categoria`
+-- Volcado de datos para la tabla `categoria`
 --
 
 INSERT INTO `categoria` (`id`, `nombre`, `descripcion`) VALUES
@@ -46,7 +45,7 @@ INSERT INTO `categoria` (`id`, `nombre`, `descripcion`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `cliente`
+-- Estructura de tabla para la tabla `cliente`
 --
 
 CREATE TABLE `cliente` (
@@ -56,37 +55,22 @@ CREATE TABLE `cliente` (
   `telefono` varchar(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `cliente`
---
-
-INSERT INTO `cliente` (`id`, `nombre`, `direccion`, `telefono`) VALUES
-(1, 'Miguel', 'Av. Roble verde #15 col. Delfines', NULL);
-
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pedido`
+-- Estructura de tabla para la tabla `pedido`
 --
 
 CREATE TABLE `pedido` (
   `id` int(11) NOT NULL,
   `fecha` datetime(6) DEFAULT NULL,
-  `cliente_id` int(11) DEFAULT NULL,
-  `estado` varchar(40) DEFAULT NULL
+  `cliente_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `pedido`
---
-
-INSERT INTO `pedido` (`id`, `fecha`, `cliente_id`, `estado`) VALUES
-(1, '2017-05-18 16:04:43.000000', 1, 'Pendiente');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pedido_producto`
+-- Estructura de tabla para la tabla `pedido_producto`
 --
 
 CREATE TABLE `pedido_producto` (
@@ -96,18 +80,10 @@ CREATE TABLE `pedido_producto` (
   `cantidad` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `pedido_producto`
---
-
-INSERT INTO `pedido_producto` (`id`, `pedido_id`, `producto_id`, `cantidad`) VALUES
-(1, 1, 1, 3),
-(2, 1, 3, 1);
-
 -- --------------------------------------------------------
 
 --
--- Table structure for table `producto`
+-- Estructura de tabla para la tabla `producto`
 --
 
 CREATE TABLE `producto` (
@@ -119,40 +95,41 @@ CREATE TABLE `producto` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `producto`
+-- Volcado de datos para la tabla `producto`
 --
 
 INSERT INTO `producto` (`id`, `nombre`, `descripcion`, `precio`, `categoria_id`) VALUES
-(1, 'Pizza hawaiana', 'Pizza con jamón y piña', '150.00', 1),
-(2, 'Pizza mexicana', 'Pizza con frijoles, chile, chorizo', '150.00', 1),
-(3, 'Refresco de 2 litros', 'Coca cola 2 litros', '30.00', 4),
-(4, 'Ensalada de la casa', 'Ensalada con las mejores hojas verdes y aderezos de la casa', '100.00', 3);
+(16, 'Mexicana', 'Con jalapeño', '125.00', 1),
+(17, 'A la carbonara', 'Queso parmesano y tocino', '42.00', 2),
+(18, 'Clasica', 'Lechuga, Jitomate y cebolla', '21.00', 3),
+(19, 'Refresco', 'Diferentes sabores', '15.00', 4),
+(20, 'Hawaiana', 'Con piña', '110.00', 1);
 
 --
--- Indexes for dumped tables
+-- Índices para tablas volcadas
 --
 
 --
--- Indexes for table `categoria`
+-- Indices de la tabla `categoria`
 --
 ALTER TABLE `categoria`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `cliente`
+-- Indices de la tabla `cliente`
 --
 ALTER TABLE `cliente`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `pedido`
+-- Indices de la tabla `pedido`
 --
 ALTER TABLE `pedido`
   ADD PRIMARY KEY (`id`),
   ADD KEY `pedido_cliente_idx` (`cliente_id`);
 
 --
--- Indexes for table `pedido_producto`
+-- Indices de la tabla `pedido_producto`
 --
 ALTER TABLE `pedido_producto`
   ADD PRIMARY KEY (`id`),
@@ -160,60 +137,60 @@ ALTER TABLE `pedido_producto`
   ADD KEY `pproducto_pedido_idx` (`pedido_id`);
 
 --
--- Indexes for table `producto`
+-- Indices de la tabla `producto`
 --
 ALTER TABLE `producto`
   ADD PRIMARY KEY (`id`),
   ADD KEY `producto_categoria_idx` (`categoria_id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT for table `categoria`
+-- AUTO_INCREMENT de la tabla `categoria`
 --
 ALTER TABLE `categoria`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
--- AUTO_INCREMENT for table `cliente`
+-- AUTO_INCREMENT de la tabla `cliente`
 --
 ALTER TABLE `cliente`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT for table `pedido`
---
-ALTER TABLE `pedido`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT for table `pedido_producto`
---
-ALTER TABLE `pedido_producto`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT for table `producto`
---
-ALTER TABLE `producto`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
--- Constraints for dumped tables
+-- AUTO_INCREMENT de la tabla `pedido`
+--
+ALTER TABLE `pedido`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT de la tabla `pedido_producto`
+--
+ALTER TABLE `pedido_producto`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+--
+-- Restricciones para tablas volcadas
 --
 
 --
--- Constraints for table `pedido`
+-- Filtros para la tabla `pedido`
 --
 ALTER TABLE `pedido`
   ADD CONSTRAINT `pedido_cliente` FOREIGN KEY (`cliente_id`) REFERENCES `cliente` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Constraints for table `pedido_producto`
+-- Filtros para la tabla `pedido_producto`
 --
 ALTER TABLE `pedido_producto`
   ADD CONSTRAINT `pproducto_pedido` FOREIGN KEY (`pedido_id`) REFERENCES `pedido` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `pproducto_producto` FOREIGN KEY (`producto_id`) REFERENCES `producto` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Constraints for table `producto`
+-- Filtros para la tabla `producto`
 --
 ALTER TABLE `producto`
   ADD CONSTRAINT `producto_categoria` FOREIGN KEY (`categoria_id`) REFERENCES `categoria` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
